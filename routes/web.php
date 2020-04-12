4a437586eec4b5e580bf83490d37ae9f57f2a696
mrmtsu <81202muramatsu@gmail.com>
2020-04-13 01:13:03 +0900

    routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'ShopController@index');
 Route::get('/search', 'ShopController@search');
